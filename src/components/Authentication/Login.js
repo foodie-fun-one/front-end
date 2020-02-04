2d428368d24bd196a894bc69c5fdab8025448ac6
Alec Dye <alecdye@gmail.com>
2020-02-04 10:27:13 -0800

    styling user login/signup forms

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -10,6 +10,7 @@ import {
     Label,
     Input
 } from "reactstrap";
+import "./Login.css";
 
 const LoginForm = () => {
     const { register, handleSubmit, errors } = useForm();
@@ -28,46 +29,43 @@ const LoginForm = () => {
             <h2>Login to your Account</h2>
             <Form className="login-form" onSubmit={handleSubmit(onSubmit)}>
                 <FormGroup>
-                    <Label>
-                        <p>Email</p>
-                        <Input
-                            type="email"
-                            name="email"
-                            placeholder="i<3food@gmail.com"
-                            ref={register({
-                                required: "Email is required",
-                                pattern: /^\S+@\S+$/i
-                            })}
-                        />
-                        {errors.email && <p>{errors.email.message}</p>}
-                    </Label>
+                    <Label for="loginEmail">Email</Label>
+                    <Input
+                        id="loginEmail"
+                        type="email"
+                        name="email"
+                        placeholder="i<3food@gmail.com"
+                        ref={register({
+                            required: "Email is required",
+                            pattern: /^\S+@\S+$/i
+                        })}
+                    />
+                    {errors.email && <p>{errors.email.message}</p>}
                 </FormGroup>
                 <FormGroup>
-                    <Label>
-                        <p>Password</p>
-                        <Input
-                            type="password"
-                            name="password"
-                            placeholder="********"
-                            ref={register({
-                                required: "Password is required",
-                                minLength: {
-                                    value: 8,
-                                    message: "Password must have at least 8 characters"
-                                }
-                            })}
-                        />
-                        {errors.password && <p>{errors.password.message}</p>}
-                    </Label>
+                    <Label for="loginPassword">Password</Label>
+                    <Input
+                        id="loginPassword"
+                        type="password"
+                        name="password"
+                        placeholder="********"
+                        ref={register({
+                            required: "Password is required",
+                            minLength: {
+                                value: 8,
+                                message: "Password must have at least 8 characters"
+                            }
+                        })}
+                    />
+                    {errors.password && <p>{errors.password.message}</p>}
                 </FormGroup>
-                <Button type="submit">
-                    Login
+                <FormGroup>
+                    <Button type="submit">
+                        Login
                 </Button>
+                </FormGroup>
+                <p>Need an account?<Link className="redirect-signup" to="/signup"> Sign Up Here</Link></p>
             </Form>
-            <Container className="redirect-form">
-                <p>Need an account?</p>
-                <Link className="redirect-signup" to="/signup">Sign Up</Link>
-            </Container>
         </Container>
     )
 }
--- src/components/Authentication/Signup.js
+++ src/components/Authentication/Signup.js
@@ -8,8 +8,11 @@ import {
     Form,
     FormGroup,
     Label,
-    Input
+    Input,
+    Col,
+    Row
 } from "reactstrap";
+import "./Signup.css";
 
 const SignUpForm = () => {
     const { register, handleSubmit, errors, watch } = useForm();
@@ -26,98 +29,91 @@ const SignUpForm = () => {
             })
     };
     return (
-        <Container className="signup-form-container">
+        <Container className="signup-form-container" fluid="">
             <h2>Create an Account</h2>
             <Form className="signup-form" onSubmit={event => event.preventDefault()}>
                 <FormGroup>
-                    <Label>
-                        <p>Email</p>
-                        <Input
-                            type="email"
-                            name="email"
-                            placeholder="I<3food@email.com"
-                            ref={register({
-                                required: "Email is required",
-                                pattern: /^\S+@\S+$/i
-                            })}
-                        />
-                        {errors.email && <p>{errors.email.message}</p>}
-                    </Label>
+                    <Label for="signupEmail">Email</Label>
+                    <Input
+                        id="signupEmail"
+                        type="email"
+                        name="email"
+                        placeholder="i<3food@email.com"
+                        ref={register({
+                            required: "Email is required",
+                            pattern: /^\S+@\S+$/i
+                        })}
+                    />
+                    {errors.email && <p>{errors.email.message}</p>}
                 </FormGroup>
                 <FormGroup>
-                    <Label>
-                        <p>Name</p>
-                        <Input
-                            type="text"
-                            name="username"
-                            placeholder="FoodChampion2"
-                            ref={register({
-                                required: "Name is required",
-                                maxLength: {
-                                    value: 50,
-                                    message: "Character limit exceeded"
-                                }
-                            })}
-                        />
-                        {errors.username && <p>{errors.username.message}</p>}
-
-                    </Label>
+                    <Label for="signupName">Name</Label>
+                    <Input
+                        id="signupName"
+                        type="text"
+                        name="username"
+                        placeholder="FoodChampion2"
+                        ref={register({
+                            required: "Name is required",
+                            maxLength: {
+                                value: 50,
+                                message: "Character limit exceeded"
+                            }
+                        })}
+                    />
+                    {errors.username && <p>{errors.username.message}</p>}
                 </FormGroup>
                 <FormGroup>
-                    <Label>
-                        <p>Password</p>
-                        <Input
-                            type="password"
-                            name="password"
-                            placeholder="********"
-                            ref={register({
-                                required: "Password is required",
-                                minLength: {
-                                    value: 8,
-                                    message: "Password must have at least 8 characters"
-                                }
-                            })}
-                        />
-                        {errors.password && <p>{errors.password.message}</p>}
-                    </Label>
+                    <Label for="signupPassword">Password</Label>
+                    <Input
+                        id="signupPassword"
+                        type="password"
+                        name="password"
+                        placeholder="********"
+                        ref={register({
+                            required: "Password is required",
+                            minLength: {
+                                value: 8,
+                                message: "Password must have at least 8 characters"
+                            }
+                        })}
+                    />
+                    {errors.password && <p>{errors.password.message}</p>}
                 </FormGroup>
                 <FormGroup>
-                    <Label>
-                        <p>Confirm Password</p>
-                        <Input
-                            type="password"
-                            name="password_confirm"
-                            placeholder="********"
-                            ref={register({
-                                validate: value =>
-                                    value === password.current || "Passwords must be the same"
-                            })}
-                        />
-                        {errors.password_confirm && <p>{errors.password_confirm.message}</p>}
-                    </Label>
+                    <Label for="signupConfirmPassword">Confirm Password</Label>
+                    <Input
+                        id="signupConfirmPassword"
+                        type="password"
+                        name="password_confirm"
+                        placeholder="********"
+                        ref={register({
+                            validate: value =>
+                                value === password.current || "Passwords must be the same"
+                        })}
+                    />
+                    {errors.password_confirm && <p>{errors.password_confirm.message}</p>}
                 </FormGroup>
                 <FormGroup>
-                    <Label>
-                        <p>Location</p>
-                        <Input
-                            type="text"
-                            name="location"
-                            placeholder="Boston"
-                            ref={register({
-                                required: "Location is required"
-                            })}
-                        />
-                        {errors.location && <p>{errors.location.message}</p>}
-                    </Label>
+                    <Label for="signupLocation">Location</Label>
+                    <Input
+                        id="signupLocation"
+                        type="text"
+                        name="location"
+                        placeholder="Boston"
+                        ref={register({
+                            required: "Location is required"
+                        })}
+                    />
+                    {errors.location && <p>{errors.location.message}</p>}
                 </FormGroup>
-                <Button type="submit" onClick={handleSubmit(onSubmit)}>
-                    Submit
-                </Button>
+                <FormGroup>
+                    <Button type="submit" onClick={handleSubmit(onSubmit)}>
+                        Create
+                        </Button>
+                </FormGroup>
+                <p>Already have an account?<Link className="redirect-login" to="/login"> Login Here</Link></p>
             </Form>
-            <Container className="redirect-form">
-                <p>Already have an account?</p>
-                <Link className="redirect-login" to="/login">Login</Link>
-            </Container>
         </Container>
     )
 }
